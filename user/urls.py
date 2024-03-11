f58690c243c01e3e318430e45cfcc37f73f644a5
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-03-11 10:21:10 +0300

    Added wishlist view and URL.

--- user/urls.py
+++ user/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path("profile/",profile,name='profile'),
+    path("wishlist/",wishlist,name='wishlist'),
 ]
--- user/views.py
+++ user/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def profile(request,username):
     return render(request, "pages/user/profile.html",{"username":username})
+
+
+def wishlist(request,username):
+    return render(request, "pages/user/wishlist.html",{"username":username})

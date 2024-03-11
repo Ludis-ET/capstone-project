c670d5edb6b2001099b61d58065c731ef16cefaa
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-03-11 10:56:32 +0300

    Update Django admin site title, header, and index title.

--- bookStore/urls.py
+++ bookStore/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path,include
 
+admin.site.site_title = "Djabooks site admin (DEV)"
+admin.site.site_header = "Djabooks administration"
+admin.site.index_title = "Site administration"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('core.urls')),

e57998b19c05a0c35b75f8f9f51f1f247c052678
Natnael-Negash <nnegash658@gmail.com>
2024-03-12 05:45:11 -0900

    up_dated models

--- core/models.py
+++ core/models.py
@@ -1,4 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
+# Book models to store books details
 class Book(models.Model):
-    pass
+    title = models.CharField(max_length = 200)
+    author = models.CharField(max_length=200)
+    genre = models.CharField(max_length=100)
+    status_choices = (
+        ('available', 'Available'),
+        ('borrowed', 'Borrowed'),
+    )
+    status = models.CharField(max_length=20, choices=status_choices, default='available')
+
+    class Meta:
+        unique_together = ("title", "author")
+    
+    def __str__(self):
+        return self.title
+    
+# Review models that user can review a book they borrowed   
+class Reveiw(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
+
+    def clean(self):
+        if self.book.status != 'borrowed' or self.user not in self.book.borrowed_by.all():
+            raise ValidationError("You can only review books that you have borrowed.")
+
+
--- user/models.py
+++ user/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from core.models import Book
 
-# Create your models here.
+class Borrow(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    borrowed_date = models.DateField(auto_now_add=True)
+    return_date = models.DateField(null=True, blank=True)
+
+    def clean(self):
+     # Check if the user has already borrowed 3 books excluding the current instance
+        if Borrow.objects.filter(user=self.user).exclude(pk=self.pk).count() >= 3:
+            raise ValidationError("You have already borrowed the maximum number of books.")
+    

4bd6da9db9ff1e352305ff1b1a0376ea5859db2b
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-03-12 22:13:08 +0300

    Update book model to include book file, description, history, and add validation for book size and user permissions.

--- core/models.py
+++ core/models.py
@@ -2,11 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
-# Book models to store books details
 class Book(models.Model):
     title = models.CharField(max_length = 200)
-    author = models.CharField(max_length=200)
+    author = models.ForeignKey(User,on_delete = models.PROTECT)
     genre = models.CharField(max_length=100)
+    book = models.FileField(upload_to='books/')
+    description = models.TextField()
+    history = models.PositiveIntegerField(default = 0)
     status_choices = (
         ('available', 'Available'),
         ('borrowed', 'Borrowed'),
@@ -17,9 +19,16 @@ class Book(models.Model):
         unique_together = ("title", "author")
     
     def __str__(self):
-        return self.title
+        return f'{self.title} book by {self.author.first_name}'
     
-# Review models that user can review a book they borrowed   
+    def clean(self):
+        if not self.author.is_superuser and not self.author.is_staff:
+            raise ValidationError("Only admins and superadmins can add books.")
+
+        max_size = 50 * 1024 * 1024 
+        if self.book.size > max_size:
+            raise ValidationError("Book size should be less than or equal to 50MB.")
+    
 class Reveiw(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)

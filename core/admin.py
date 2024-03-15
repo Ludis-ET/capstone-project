4f0e6e4876da9ccdcdb882de56c83ce8e0e8a0b0
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-03-15 12:47:09 +0300

    Update GenreAdmin to display book list and use exact genre ID in query params.

--- core/admin.py
+++ core/admin.py
@@ -5,16 +5,13 @@ from . import models
 
 @admin.register(models.Genre)
 class GenreAdmin(admin.ModelAdmin):
-    list_display = ['name_link']
-    search_fields = ['name__istartswith']
-    def name_link(self, genre):
-        url = (reverse('admin:core_Book_changelist')
-            + '?'
-            + urlencode({
-                'genre__id': str(genre.id)
-            })
-        )
-        format_html('<a href={}>{}</a>, url, genre')
+    list_display = ['name', 'book_list']
+    search_fields = ['name']
+    def book_list(self, genre):
+        url = reverse('admin:core_book_changelist')
+        query_params = urlencode({'genres__id__exact': genre.id})
+        url += f'?{query_params}'
+        return format_html('<a href="{}">{}</a>', url, f'{genre.name} Books')
 
 @admin.register(models.Book)
 class BookAdmin(admin.ModelAdmin):

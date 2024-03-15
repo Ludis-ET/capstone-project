404f72c0e05832c6121c161d72868c8a5c657afb
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-03-15 13:30:44 +0300

    Remove unused import statements.

--- core/forms.py
+++ core/forms.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from user.models import CustomUser
@@ -7,14 +6,4 @@ class CustomUserCreationForm(UserCreationForm):
 
     class Meta(UserCreationForm.Meta):
         model = CustomUser
-=======
-from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from user.models import CustomUser
-
-class CustomUserCreationForm(UserCreationForm):
-
-    class Meta(UserCreationForm.Meta):
-        model = CustomUser
->>>>>>> 228ff23407da50740c0b2fe232f2e0ee74b54fd5
         fields = ('first_name', 'username', 'password1', 'password2',)

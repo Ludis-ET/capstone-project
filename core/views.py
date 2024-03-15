94b6de2050e8b2935d592dc125a5a33ea11c1115
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-03-15 14:50:56 +0300

    Update views.py and base.html. - Add user registration functionality. - Add book detail page. - Update message display style.

--- core/views.py
+++ core/views.py
@@ -1,15 +1,31 @@
-from django.shortcuts import render
-from .models import *
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from .models import Book
 
 def index(request):
-    
-    return render(request, "pages/Home/home.html",{})
-
+    return render(request, "pages/Home/home.html", {})
 
 def shelf(request):
     books = Book.objects.all()
-    return render(request, "pages/shelf/shelf.html",{"books":books})
+    return render(request, "pages/shelf/shelf.html", {"books": books})
 
+def book(request, id):
+    return render(request, "pages/shelf/BookDetail.html", {})
 
-def book(request,id):
-    return render(request, "pages/shelf/BookDetail.html",{})
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'{form.cleaned_data.get("first_name")} account created successfully!')
+            return redirect('index')
+        else:
+            for field, errors in form.errors.items():
+                for error in errors:
+                    messages.error(request, f'{error}')
+            return redirect('index')  # Redirect to index if registration fails
+    else:
+        messages.error(request, 'nothing to register')
+    return redirect('index')  # Redirect to index if request method is not POST

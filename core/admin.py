13b90a754dba2d5670260c8c4d0384d0c8aca44d
me-ron <meronkiflayyy@gmail.com>
2024-03-15 11:14:51 +0300

    registering Genre

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import *
+from django.utils.html import format_html
+from django.urls import reverse
+from . import models
 
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    exclude = ('history',)
-    
-admin.site.register(Genre)
-admin.site.register(Testimony)
+@admin.register(models.Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ['name_link']
+    search_fields = ['name__istartswith']
+    def name_link(self, genre):
+        url = "http://google.com"
+        format_html('<a href={}>{}</a>, url, genre')
+

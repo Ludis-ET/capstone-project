1547fe9c7312d37186cdae48d0fcac4be3bedc55
me-ron <meronkiflayyy@gmail.com>
2024-03-15 11:40:16 +0300

    excluded view and status from admin in Book

--- core/admin.py
+++ core/admin.py
@@ -21,5 +21,6 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ['title', 'author','view', 'status']
     list_filter = ['genres','status', 'author']
     search_fields = ['title__istartswith']
+    exclude = ('view', 'status')
 
 

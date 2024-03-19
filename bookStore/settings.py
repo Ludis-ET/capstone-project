caa8828eae0a5a7035ff7dbf2c4b5af0b3ae93a3
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-03-19 16:49:25 +0300

    installing whitenoise

--- bookStore/settings.py
+++ bookStore/settings.py
@@ -40,6 +40,8 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',

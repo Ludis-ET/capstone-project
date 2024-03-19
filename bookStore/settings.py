51f28397f31d40dcdcea46831ad405c3e3ee64cd
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-03-19 17:19:59 +0300

    giving up on vercel

--- bookStore/settings.py
+++ bookStore/settings.py
@@ -52,7 +52,7 @@ ROOT_URLCONF = 'bookStore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

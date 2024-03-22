5766428b02bab69293e142dcdd43f5b925312a30
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-03-22 20:53:46 +0300

    Change STATIC\_URL from '/static/' to '/staticfiles/'; update STATICFILES\_DIRS and STATIC\_ROOT accordingly
    Explanation:
    
    - The code diff shows the modification in the settings.py file of a Django project named bookStore.
    - The variable STATIC\_URL has been changed from '/static/' to '/staticfiles/'.
    - Additionally, the variable STATICFILES\_DIRS has been updated to include the new location of static files, and STATIC\_ROOT has been set to the absolute path of the new location for collecting static files in a deploy scenario.
    - These changes are likely made to avoid conflicts with other applications or to follow a specific project's naming conventions.

--- bookStore/settings.py
+++ bookStore/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 # Static files (CSS, JavaScript, images)
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
